228b5943fef371a55ca547c5b67e123065496da3
Jgump <gangmail@icloud.com>
2022-07-08 19:54:52 +0800

    将资源文件放入文件夹

--- Package.swift
+++ Package.swift
@@ -20,16 +20,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
-        .target(name: "LotteryData",resources: [
-            .process("2003.plist"),.process("2004.plist"),.process("2005.plist"),
-            .process("2006.plist"),.process("2007.plist"),.process("2008.plist"),
-            .process("2009.plist"),.process("2010.plist"),.process("2011.plist"),
-            .process("2012.plist"),.process("2013.plist"),.process("2014.plist"),
-            .process("2015.plist"),.process("2016.plist"),.process("2017.plist"),
-            .process("2018.plist"),.process("2019.plist"),.process("2020.plist"),
-            .process("2021.plist"),.process("2022.plist")
-            
-        ]),
+        .target(name: "LotteryData",resources: [.process("Plists/")]),
+//            .process("2003.plist"),.process("2004.plist"),.process("2005.plist"),
+//            .process("2006.plist"),.process("2007.plist"),.process("2008.plist"),
+//            .process("2009.plist"),.process("2010.plist"),.process("2011.plist"),
+//            .process("2012.plist"),.process("2013.plist"),.process("2014.plist"),
+//            .process("2015.plist"),.process("2016.plist"),.process("2017.plist"),
+//            .process("2018.plist"),.process("2019.plist"),.process("2020.plist"),
+//            .process("2021.plist"),.process("2022.plist")
+//
+//        ]),
        
         .testTarget(name: "LotteryDataTests",dependencies: ["LotteryData"]),
     ]
