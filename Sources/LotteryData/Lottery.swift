74b2c7dc0bd1c1db800a0638a8441bb8c3d6634e
Jgump <182421315@qq.com>
2022-07-19 07:19:15 +0800

    new Model of Lottery

---
+++ Sources/LotteryData/Lottery.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jgump on 2022/7/19.
+//
+
+import Foundation
+
+public struct Lottery{
+    let issue:String
+    let date:String
+    let nums:String
+    
+    
+    public  init?(dir:Dictionary<String,Any>){
+        guard let issue = dir["issue"] as? String,
+        let date = dir["date"] as? String,
+            let nums = dir["nums"] as? String else {return nil}
+        self.issue  = issue
+        self.date = date
+        self.nums = nums
+    }
+    
+}
